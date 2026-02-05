a9e73c62d48c04b429c5fe8600a7245dacbd1ab9
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-02-05 15:01:41 +0700

    Commit

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -13,6 +13,7 @@ $current = basename($_SERVER['PHP_SELF']);
   <!-- Menu -->
   <ul class="sidebar-menu">
 
+    <!-- Dashboard -->
     <li>
       <a href="dashboard.php" class="<?= $current=='dashboard.php'?'active':'' ?>">
         <i class="fa-solid fa-house icon"></i>
@@ -20,6 +21,7 @@ $current = basename($_SERVER['PHP_SELF']);
       </a>
     </li>
 
+    <!-- Produk -->
     <li>
       <a href="produk.php" class="<?= $current=='produk.php'?'active':'' ?>">
         <i class="fa-solid fa-truck-moving icon"></i>
@@ -27,6 +29,7 @@ $current = basename($_SERVER['PHP_SELF']);
       </a>
     </li>
 
+    <!-- Artikel -->
     <li>
       <a href="artikel.php" class="<?= $current=='artikel.php'?'active':'' ?>">
         <i class="fa-regular fa-newspaper icon"></i>
@@ -34,6 +37,7 @@ $current = basename($_SERVER['PHP_SELF']);
       </a>
     </li>
 
+    <!-- Pesan -->
     <li>
       <a href="pesan.php" class="<?= $current=='pesan.php'?'active':'' ?>">
         <i class="fa-solid fa-comments icon"></i>
@@ -41,10 +45,43 @@ $current = basename($_SERVER['PHP_SELF']);
       </a>
     </li>
 
+    <!-- Simulasi -->
     <li>
       <a href="simulasi.php" class="<?= $current=='simulasi.php'?'active':'' ?>">
         <i class="fa-solid fa-calculator icon"></i>
         <span>Simulasi Kredit</span>
+      </a>
+    </li>
+
+    <!-- User Admin -->
+    <li>
+      <a href="user_admin.php" class="<?= $current=='user_admin.php'?'active':'' ?>">
+        <i class="fa-solid fa-users-gear icon"></i>
+        <span>User Admin</span>
+      </a>
+    </li>
+
+    <!-- Leads -->
+    <li>
+      <a href="leads.php" class="<?= $current=='leads.php'?'active':'' ?>">
+        <i class="fa-solid fa-user-plus icon"></i>
+        <span>Leads Customer</span>
+      </a>
+    </li>
+
+    <!-- Stock -->
+    <li>
+      <a href="stock.php" class="<?= $current=='stock.php'?'active':'' ?>">
+        <i class="fa-solid fa-warehouse icon"></i>
+        <span>Stock Unit</span>
+      </a>
+    </li>
+
+    <!-- Delivery -->
+    <li>
+      <a href="delivery.php" class="<?= $current=='delivery.php'?'active':'' ?>">
+        <i class="fa-solid fa-truck-ramp-box icon"></i>
+        <span>Delivery Order</span>
       </a>
     </li>
 
