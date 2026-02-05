a747377a541978007c255c45a1770aeefd0f8d34
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-02-05 14:10:34 +0700

    Commit

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -22,7 +22,7 @@ $current = basename($_SERVER['PHP_SELF']);
 
     <li>
       <a href="produk.php" class="<?= $current=='produk.php'?'active':'' ?>">
-        <i class="fa-solid fa-crane icon"></i>
+        <i class="fa-solid fa-truck-moving icon"></i>
         <span>Produk</span>
       </a>
     </li>
