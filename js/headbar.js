814f83ebbf185e65fae73bd267ff43bfa3f785e3
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-02-14 01:19:52 +0700

    Commit

--- js/headbar.js
+++ js/headbar.js
@@ -2,3 +2,27 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector(".header");
   header.classList.toggle("scrolled", window.scrollY > 50);
 });
+
+// Scroll Headbar
+let lastScroll = 0;
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  // Background aktif
+  if (currentScroll > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+
+  // Hide saat scroll turun
+  if (currentScroll > lastScroll && currentScroll > 150) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll;
+});
