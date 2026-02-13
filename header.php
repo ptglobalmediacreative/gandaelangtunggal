4bcf671af4096812d25277986580ca67264f87ff
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-02-14 01:05:48 +0700

    Commit

--- header.php
+++ header.php
@@ -12,6 +12,8 @@
     <!-- Favicon -->
     <link rel="icon" type="image/webp" href="/images/favicon.webp">
 
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
