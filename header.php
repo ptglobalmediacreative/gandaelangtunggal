1f097d82ae40de2c4314a2a3d2fe4399901bfad1
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-02-28 17:14:44 +0700

    Commit

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
-<?php
+<!-- <?php
 $maintenance = true;
 
 if ($maintenance && basename($_SERVER['PHP_SELF']) != 'maintenance.php') {
     header("Location: /maintenance.php");
     exit;
 }
-?>
+?> -->
 
 <!DOCTYPE html>
 <html lang="id">
