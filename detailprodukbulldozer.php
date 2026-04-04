e407695351e11fc4cb570c7732610c9bbc1aa5d6
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-04-04 11:46:07 +0700

    Commit

--- detailprodukbulldozer.php
+++ detailprodukbulldozer.php
@@ -267,10 +267,21 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 </div>
 
 <div class="pd-menu-nav">
+
+<?php if (!empty($features)): ?>
 <a href="#pd-features">Features</a>
+<?php endif; ?>
+
+<?php if (!empty($group)): ?>
 <a href="#pd-specifications">Specifications</a>
+<?php endif; ?>
+
+<?php if (!empty($gallery)): ?>
 <a href="#pd-gallery">Gallery</a>
+<?php endif; ?>
+
 <a href="#pd-recommended">Recommended Equipment</a>
+
 </div>
 
 </div>
@@ -279,6 +290,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
 
 <!-- FEATURES -->
+<?php if (!empty($features)): ?>
 <section id="pd-features" class="pd-section">
 
 <h2>FEATURES</h2>
@@ -305,9 +317,11 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 </div>
 
 </section>
+<?php endif; ?>
 
 
 <!-- SPEC -->
+<?php if (!empty($group)): ?>
 <section id="pd-specifications" class="pd-section pd-gray">
 
 <h2>SPECIFICATIONS</h2>
@@ -334,9 +348,11 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 <?php endforeach; ?>
 
 </section>
+<?php endif; ?>
 
 
 <!-- GALLERY -->
+<?php if (!empty($gallery)): ?>
 <section id="pd-gallery" class="pd-section">
 
 <h2>GALLERY</h2>
@@ -362,6 +378,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 </div>
 
 </section>
+<?php endif; ?>
 
 
 <!-- RECOMMENDED -->
@@ -373,7 +390,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
 <?php foreach($rec as $r): ?>
 
-<a href="/detailprodukbulldozer.php?slug=<?=$r['slug']?>" class="pd-card">
+<a href="/detailprodukexcavator.php?slug=<?=$r['slug']?>" class="pd-card">
 
 <img src="/images/uploads/produk/<?=$r['gambar']?>">
 
