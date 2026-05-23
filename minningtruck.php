f02426c64a4112bf886aa7858263ae83ce8ed300
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-05-23 20:36:07 +0700

    Commit

--- minningtruck.php
+++ minningtruck.php
@@ -307,7 +307,7 @@ $currentUrl = "https://gandaelang.co.id/minningtruck.php";
 </header>
 
 <!-- ================= HERO SECTION ================= -->
-<section class="hero hero-image" style="background: url('/images/minningtruck.webp') center / cover no-repeat;">
+<section class="hero hero-image" style="background: url('/images/minningtruck.jpg') center / cover no-repeat;">
     <div class="hero-overlay"></div>
     <div class="hero-content">
         <div class="hero-breadcrumb">
