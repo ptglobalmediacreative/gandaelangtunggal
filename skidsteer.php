82f7e8a6042d403f367e713f5b6b789a756462ef
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-05-23 21:00:03 +0700

    COmmit

--- skidsteer.php
+++ skidsteer.php
@@ -11,7 +11,7 @@ $stmt = $pdo->prepare("
         p.gambar
     FROM produk p
     WHERE p.status = 'aktif'
-      AND p.category_id = 7
+      AND p.category_id = 11
     ORDER BY p.id DESC
 ");
 $stmt->execute();
