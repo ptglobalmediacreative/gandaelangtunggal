2745b0930556a422569453ad905ce4864d896516
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-05-23 21:14:20 +0700

    Commit

--- detailprodukpaver.php
+++ detailprodukpaver.php
@@ -47,7 +47,7 @@ $hero = $q->fetch();
 
 $heroImage = $hero
     ? "/images/uploads/produk/" . $hero['image']
-    : "/images/hero.jpg";
+    : "/images/paver.jpg";
 
 
 /* ================= FEATURES ================= */
