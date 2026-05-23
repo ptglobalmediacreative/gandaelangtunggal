1cd76a4506772b95cf7b35e9a3d7bf587340768e
PTGlobalMediaCreative <ptglobalmediacreative@gmail.com>
2026-05-23 21:27:48 +0700

    Commit

--- detailprodukbackhoeloader.php
+++ detailprodukbackhoeloader.php
@@ -272,7 +272,9 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 <a href="#pd-gallery">Gallery</a>
 <?php endif; ?>
 
+<?php if (!empty($rec)): ?>
 <a href="#pd-recommended">Recommended Equipment</a>
+<?php endif; ?>
 
 </div>
 
@@ -374,6 +376,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 
 
 <!-- RECOMMENDED -->
+<?php if (!empty($rec)): ?>
 <section id="pd-recommended" class="pd-section pd-gray">
 
 <h2>RECOMMENDED EQUIPMENT</h2>
@@ -395,6 +398,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 </div>
 
 </section>
+<?php endif; ?>
 
 
 <?php include $_SERVER['DOCUMENT_ROOT']."/footer.php"; ?>
